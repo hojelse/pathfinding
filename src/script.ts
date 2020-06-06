45417050e9cfb9f0e66919d8591e5c1433ff34b7
hojelse <k1@hojelse.dk>
2020-06-06 22:08:12 +0200

    Testing transfer

--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,10 @@
-let g = new Graph(2);
-console.log(g);
+
+let graph = new Graph(2);
+
+let dist:number[] = [];
+function dijkstra(graph:Graph, source:number) {
+  dist[source] = 0;
+  // graph.forEach(element => {
+    
+  // });
+}
