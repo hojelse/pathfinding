978c1f334b240c81f58f868b4fa9f733cc0296c7
hojelse <k1@hojelse.dk>
2020-06-27 23:11:07 +0200

    Extract GridGraph to own file

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -119,28 +119,6 @@ function coordToNodeIDGeneral(x:number, y:number, cols:number):number {
   return x + y*cols;
 }
 
-class GridGraph extends Graph {
-  rows:number;
-  cols:number;
-
-  constructor(rows:number, columns:number) {
-    super(rows*columns);
-    this.rows = rows;
-    this.cols = columns;
-  }
-
-  coordsToId(col:number, row:number):number {
-    return col + row * this.cols;
-  }
-
-  idToCoords(id:number):{col:number,row:number} {
-    let col = id % this.cols;
-    let row = Math.floor(id / this.cols);
-    return {col: col,row: row};
-  }
-
-}
-
 class Board {
   
 }
