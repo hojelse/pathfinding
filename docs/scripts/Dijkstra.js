3187c06173f465770eef816cbfde03ff5ee49081
hojelse <k1@hojelse.dk>
2020-06-30 15:59:08 +0200

    Change dijkstra fields to public

--- docs/scripts/Dijkstra.js
+++ docs/scripts/Dijkstra.js
@@ -1,61 +1,43 @@
 "use strict";
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to set private field on non-instance");
-    }
-    privateMap.set(receiver, value);
-    return value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
-};
-var _distTo, _edgeTo, _pq;
 // Pathfinding algorithm Dijkstra that runs on a Graph
 class Dijkstra {
     constructor(graph, startVertex) {
-        _distTo.set(this, void 0);
-        _edgeTo.set(this, void 0);
-        _pq.set(this, void 0);
-        __classPrivateFieldSet(this, _edgeTo, new Array(graph.V));
-        __classPrivateFieldSet(this, _distTo, new Array(graph.V));
-        __classPrivateFieldSet(this, _pq, new MinPQ(graph.V));
+        this.edgeTo = new Array(graph.V);
+        this.distTo = new Array(graph.V);
+        this.pq = new MinPQ(graph.V);
         for (let v = 0; v < graph.V; v++)
-            __classPrivateFieldGet(this, _distTo)[v] = Infinity;
-        __classPrivateFieldGet(this, _distTo)[startVertex] = 0;
-        __classPrivateFieldGet(this, _pq).insert(startVertex, 0);
-        while (!__classPrivateFieldGet(this, _pq).isEmpty())
-            this.relax(graph, __classPrivateFieldGet(this, _pq).popMin());
+            this.distTo[v] = Infinity;
+        this.distTo[startVertex] = 0;
+        this.pq.insert(startVertex, 0);
+        while (!this.pq.isEmpty())
+            this.relax(graph, this.pq.popMin());
     }
     relax(graph, fromVertex) {
         for (const edge of graph.getNeigborhoodOf(fromVertex)) {
             let toVertex = edge.to;
-            if (__classPrivateFieldGet(this, _distTo)[toVertex] > __classPrivateFieldGet(this, _distTo)[fromVertex] + edge.weight) {
-                __classPrivateFieldGet(this, _distTo)[toVertex] = __classPrivateFieldGet(this, _distTo)[fromVertex] + edge.weight;
-                __classPrivateFieldGet(this, _edgeTo)[toVertex] = edge;
-                if (__classPrivateFieldGet(this, _pq).contains(toVertex))
-                    __classPrivateFieldGet(this, _pq).changeKey(toVertex, __classPrivateFieldGet(this, _distTo)[toVertex]);
+            if (this.distTo[toVertex] > this.distTo[fromVertex] + edge.weight) {
+                this.distTo[toVertex] = this.distTo[fromVertex] + edge.weight;
+                this.edgeTo[toVertex] = edge;
+                if (this.pq.contains(toVertex))
+                    this.pq.changeKey(toVertex, this.distTo[toVertex]);
                 else
-                    __classPrivateFieldGet(this, _pq).insert(toVertex, __classPrivateFieldGet(this, _distTo)[toVertex]);
+                    this.pq.insert(toVertex, this.distTo[toVertex]);
             }
         }
     }
-    distTo(vertex) {
-        return __classPrivateFieldGet(this, _distTo)[vertex];
+    getDistTo(vertex) {
+        return this.distTo[vertex];
     }
     hasPathTo(vertex) {
-        return __classPrivateFieldGet(this, _distTo)[vertex] < Infinity;
+        return this.distTo[vertex] < Infinity;
     }
-    pathTo(vertex) {
+    getPathTo(vertex) {
         if (!this.hasPathTo(vertex))
             return null;
         let path = new Array();
-        for (let edge = __classPrivateFieldGet(this, _edgeTo)[vertex]; edge != null; edge = __classPrivateFieldGet(this, _edgeTo)[edge.from]) {
+        for (let edge = this.edgeTo[vertex]; edge != null; edge = this.edgeTo[edge.from]) {
             path.push(edge);
         }
         return path;
     }
 }
-_distTo = new WeakMap(), _edgeTo = new WeakMap(), _pq = new WeakMap();
--- src/scripts/Dijkstra.ts
+++ src/scripts/Dijkstra.ts
@@ -1,45 +1,45 @@
 // Pathfinding algorithm Dijkstra that runs on a Graph
 class Dijkstra {
-  #distTo:number[];
-  #edgeTo:Edge[];
-  #pq:MinPQ;
+  distTo:number[];
+  edgeTo:Edge[];
+  pq:MinPQ;
 
   constructor(graph:Graph, startVertex:number) {
-    this.#edgeTo = new Array<Edge>(graph.V);
-    this.#distTo = new Array<number>(graph.V);
-    this.#pq = new MinPQ(graph.V);
+    this.edgeTo = new Array<Edge>(graph.V);
+    this.distTo = new Array<number>(graph.V);
+    this.pq = new MinPQ(graph.V);
 
-    for (let v = 0; v < graph.V; v++) this.#distTo[v] = Infinity;
-    this.#distTo[startVertex] = 0;
+    for (let v = 0; v < graph.V; v++) this.distTo[v] = Infinity;
+    this.distTo[startVertex] = 0;
 
-    this.#pq.insert(startVertex, 0);
-    while(!this.#pq.isEmpty()) this.relax(graph, this.#pq.popMin());
+    this.pq.insert(startVertex, 0);
+    while(!this.pq.isEmpty()) this.relax(graph, this.pq.popMin());
   }
 
   relax(graph:Graph, fromVertex:number) {
     for (const edge of graph.getNeigborhoodOf(fromVertex)) {
       let toVertex:number = edge.to;
-      if (this.#distTo[toVertex] > this.#distTo[fromVertex] + edge.weight) {
-        this.#distTo[toVertex] = this.#distTo[fromVertex] + edge.weight;
-        this.#edgeTo[toVertex] = edge;
-        if (this.#pq.contains(toVertex)) this.#pq.changeKey(toVertex, this.#distTo[toVertex]);
-        else this.#pq.insert(toVertex, this.#distTo[toVertex]);
+      if (this.distTo[toVertex] > this.distTo[fromVertex] + edge.weight) {
+        this.distTo[toVertex] = this.distTo[fromVertex] + edge.weight;
+        this.edgeTo[toVertex] = edge;
+        if (this.pq.contains(toVertex)) this.pq.changeKey(toVertex, this.distTo[toVertex]);
+        else this.pq.insert(toVertex, this.distTo[toVertex]);
       }
     }
   }
   
-  distTo(vertex:number):number {
-    return this.#distTo[vertex];
+  getDistTo(vertex:number):number {
+    return this.distTo[vertex];
   }
 
   hasPathTo(vertex:number) {
-    return this.#distTo[vertex] < Infinity;
+    return this.distTo[vertex] < Infinity;
   }
 
-  pathTo(vertex:number):Array<Edge> {
+  getPathTo(vertex:number):Array<Edge> {
     if(!this.hasPathTo(vertex)) return null;
     let path = new Array<Edge>();
-    for (let edge = this.#edgeTo[vertex]; edge != null; edge = this.#edgeTo[edge.from]) {
+    for (let edge = this.edgeTo[vertex]; edge != null; edge = this.edgeTo[edge.from]) {
       path.push(edge);
     }
     return path;
